f0ab97e855c88fb7d385c1be0ce06971a6708845
vshchq <62289858+vshchq@users.noreply.github.com>
2021-05-03 20:44:49 +0200

    Poprawione UI i zmiana jezyka na polski

---
+++ Assets/Samples/2D Pixel Perfect/2.1.0/Sample Scenes and Extras/Scenes/Scripts/CameraManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera ordinaryCamera;
+
+    private bool isPixelPerfect;
+
+    void Awake()
+    {
+        isPixelPerfect = true;
+        ValidateCameras(isPixelPerfect);
+    }
+
+    public void TogglePixelPerfect(bool value)
+    {
+        isPixelPerfect = value;
+        ValidateCameras(isPixelPerfect);
+    }
+
+    public void ValidateCameras(bool value)
+    {
+        if (value)
+        {
+            ordinaryCamera.gameObject.SetActive(false);
+        }
+        else
+        {
+            ordinaryCamera.gameObject.SetActive(true);
+        }
+    }
+}
